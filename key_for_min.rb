547a3a081e6c64db6a21fddefec370e18c37f582
Yvonne Chen <yvonnechen1991@gmail.com>
2020-12-29 05:34:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  #smallest = hash.first 
+  smallest_key, smallest_value = hash.first
+  #smallest_key = hash.first[0]
+  name_hash.each do |name, number|
+    if number < smallest_value 
+      smallest_value = number
+    end 
+  end 
+  smallest_key
 end
